dac55c7f3d99091f17c18529276273d79286687e
Mohammad Nowaf <muhammadnowaf2608@gmail.com>
2022-07-20 18:27:39 +0700

    adding container on detailscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projectawal/DetailScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(),
-      
+      home: DetailScreen(),      
     );
   }
 }
