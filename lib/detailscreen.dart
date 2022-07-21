db1ad4436c2d324f6e4c0de9b5a1ec2bed128dab
Mohammad Nowaf <muhammadnowaf2608@gmail.com>
2022-07-21 16:50:06 +0700

    adding images assets

--- lib/detailscreen.dart
+++ lib/detailscreen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
 
@@ -12,9 +12,10 @@ class DetailScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Image.asset('images/farm-house.jpg'),
             Container(
-              margin: EdgeInsets.only(top: 16.0),
-              child: Text(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
                 'Farm House Lembang',
                 textAlign: TextAlign.center,
                 style: TextStyle(
@@ -23,31 +24,31 @@ class DetailScreen extends StatelessWidget {
                 ),),
             ),
             Container(
-              margin: EdgeInsets.symmetric(vertical: 16.0),
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Column(
                     children: <Widget>[
                       Icon(Icons.calendar_today, color: Colors.green[500],),
-                      SizedBox(height: 8.0,),
-                      Text('Open Everyday'),
+                      const SizedBox(height: 8.0,),
+                      const Text('Open Everyday'),
                     ],
                   ),
 
                   Column(
                     children: <Widget>[
                       Icon(Icons.access_time, color: Colors.green[500],),
-                      SizedBox(height: 8.0,),
-                      Text('09:00 - 20:00'),
+                      const SizedBox(height: 8.0,),
+                      const Text('09:00 - 20:00'),
                     ],
                   ),
 
                   Column(
                     children: <Widget>[
                       Icon(Icons.monetization_on, color: Colors.green[500],),
-                      SizedBox(height: 8.0,),
-                      Text('Rp. 25.000'),
+                      const SizedBox(height: 8.0,),
+                      const Text('Rp. 25.000'),
                     ],
                   )
 
@@ -55,8 +56,8 @@ class DetailScreen extends StatelessWidget {
               ),
             ),
             Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
                 'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 16.0),
